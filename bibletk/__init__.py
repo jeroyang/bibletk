83e464f673c902a5ed188e7c4e8fdc484685b609
jeroyang <jeroyang@gmail.com>
2016-04-16 15:53:45 +0800

    remove versioneer in __init__

--- bibletk/__init__.py
+++ bibletk/__init__.py
@@ -4,12 +4,6 @@
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 from builtins import *
-import versioneer
 
 __author__ = 'Chia-Jung, Yang'
 __email__ = 'jeroyang@gmail.com'
-__version__ = versioneer.get_version()
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
