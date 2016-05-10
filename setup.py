a4d508fac3b895052f3558affd58fcce36c84e2d
jeroyang <jeroyang@gmail.com>
2016-05-10 16:31:42 +0800

    fix windows problem

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ try:
 except ImportError:
     from distutils.core import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open('README.rst', encoding='utf8') as readme_file:
     readme = readme_file.read()
 
-with open('requirements.txt') as f:
+with open('requirements.txt', encoding='utf8') as f:
     requirements = f.read().splitlines()
 
 test_requirements = [
